5d3f5fd2d8fc4628adedcfda4bc3d63457f82d7d
WN86 <cantbackdown86@gmail.com>
2021-01-24 16:21:00 +0800

    init, function: create, index api, passport auth, ajax request.

---
+++ public/js/post.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function($){
+    //----- Open model CREATE -----//
+    jQuery('#btn-add').click(function () {
+        jQuery('#btn-save').val("add");
+        jQuery('#myForm').trigger("reset");
+        jQuery('#formModal').modal('show');
+    });
+
+    // CREATE
+    $("#btn-save").click(function (e) {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': jQuery('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        e.preventDefault();
+        var formData = {
+            user_id: jQuery('#user_id').val(),
+            title: jQuery('#title').val(),
+            description: jQuery('#description').val(),
+        };
+        var state = jQuery('#btn-save').val();
+        var type = "POST";
+        var post_id = jQuery('#post_id').val();
+        var ajaxurl = 'post';
+
+        $.ajax({
+            type: type,
+            url: ajaxurl,
+            data: formData,
+            dataType: 'json',
+            success: function (data) {
+                var post = '<tr id="post' + data.post.id + '"><td>' + data.post.id + '</td><td>' + data.post.title + '</td><td>' + data.post.description + '</td></tr>';
+                if (state == "add") {
+                    jQuery('#posts-list').append(post);
+                    console.log(post);
+                } else {
+                    jQuery("#post" + post_id).replaceWith(post);
+                }
+                jQuery('#myForm').trigger("reset");
+                jQuery('#formModal').modal('hide')
+            },
+            error: function (data) {
+                console.log(data);
+            }
+        });
+    });
+});
